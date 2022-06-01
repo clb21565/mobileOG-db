e81bfb87f7c6a1f159ddf195e01e8ccac5944f29
jamesm224 <86495895+jamesm224@users.noreply.github.com>
2022-06-01 19:44:25 -0400

    Update mobileOGs-pl-kyanite.sh

--- mobileOG-pl/mobileOGs-pl-kyanite.sh
+++ mobileOG-pl/mobileOGs-pl-kyanite.sh
@@ -36,6 +36,11 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    -m|--metadata)
+      METADATA="$2"
+      shift # past argument
+      shift # past value
+      ;;
      esac
 done
 
@@ -51,6 +56,6 @@ for sample in $samples
 do
 prodigal -i ${sample} -p meta -a ${sample}.faa
 diamond blastp -q ${sample}.faa --db ${DIAMOND} --outfmt 6 stitle qtitle pident bitscore slen evalue qlen sstart send qstart qend -k $KVALUE -o ${sample}.tsv -e $ESCORE --query-cover $QUERYSCORE --id $PIDENTVALUE
-python mobileOGs-pl.py --o ${sample} --i ${sample}.tsv --m ${DIAMOND}
+python mobileOGs-pl.py --o ${sample} --i ${sample}.tsv -m ${METADATA}
 done
 
